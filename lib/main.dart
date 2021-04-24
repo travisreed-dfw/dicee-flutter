efbeda35fb4f52f0168b2047e2d10a1a2ff718ec
Travis Reed <traviswreed2@gmail.com>
2021-04-24 09:29:55 -0500

    Final build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,105 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int addedDice = 2;
+  int rollsMade = 0;
+
+  void changeDiceFace() {
+    setState(() {
+      rollsMade++;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      addedDice = leftDiceNumber + rightDiceNumber;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Text(
+                  '$leftDiceNumber',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 70,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(
+                  '$rightDiceNumber',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 70,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.casino,
+                      color: Colors.red,
+                    ),
+                    title: Text(
+                      'The total rolled is $addedDice.\nYou have rolled $rollsMade times.',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.red[900],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
